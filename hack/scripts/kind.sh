4f72f0ab55d0e37ecf75b6fa0d5a8bd38ae1931d
Rafael <rafael.oliveira@wildlifestudios.com>
2022-03-15 09:32:14 -0300

    chore: fix the version of kind image to v1.21.10 in script for dev environment

--- hack/scripts/kind.sh
+++ hack/scripts/kind.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $(kind get clusters | grep "${1}" -q) -eq 1 ]]; then
-  kind create cluster --name "${1}"
+  kind create cluster --name "${1}" --image=kindest/node:v1.21.10@sha256:84709f09756ba4f863769bdcabe5edafc2ada72d3c8c44d6515fc581b66b029c
 else
   kind delete cluster --name "${1}"
-  kind create cluster --name "${1}"
+  kind create cluster --name "${1}" --image=kindest/node:v1.21.10@sha256:84709f09756ba4f863769bdcabe5edafc2ada72d3c8c44d6515fc581b66b029c
 fi
