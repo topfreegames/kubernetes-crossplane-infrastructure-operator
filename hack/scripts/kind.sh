63d6487f8f93c633296f1db38f84bc355888f2b3
Leonardo Godoy <leonardo.godoy@wildlifestudios.com>
2022-03-23 19:39:33 -0300

    chore: use custom image to deploy AWS Provider

--- hack/scripts/kind.sh
+++ hack/scripts/kind.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $(kind get clusters | grep "${1}" -q) -eq 1 ]]; then
-  kind create cluster --name "${1}"
+  kind create cluster --name "${1}" --image kindest/node:v1.22.7
 else
   kind delete cluster --name "${1}"
-  kind create cluster --name "${1}"
+  kind create cluster --name "${1}" --image kindest/node:v1.22.7
 fi
