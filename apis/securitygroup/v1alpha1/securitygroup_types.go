536cc7905589e9f50bf65267495871338c715ce2
Emanuel Joivo <emanuel.joivo@wildlifestudios.com>
2022-08-15 10:16:44 -0300

    Add status print in columns

--- apis/securitygroup/v1alpha1/securitygroup_types.go
+++ apis/securitygroup/v1alpha1/securitygroup_types.go
@@ -79,7 +79,11 @@ type SecurityGroupStatus struct {
 }
 
 //+kubebuilder:object:root=true
+//+kubebuilder:resource:shortName=wsg
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description=""
+//+kubebuilder:printcolumn:name="Ready",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].status",description=""
+//+kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.conditions[?(@.type==\"Ready\")].message",description=""
 
 // SecurityGroup is the Schema for the securitygroups API
 type SecurityGroup struct {
