228a8c72c210fa548c89884a549a18cde01245b9
Lucas Telles <lucas.telles@wildlifestudios.com>
2023-02-01 16:00:49 -0300

    chore: finish ocean tests

--- pkg/spot/ocean_test.go
+++ pkg/spot/ocean_test.go
@@ -2,6 +2,7 @@ package spot
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
@@ -13,13 +14,38 @@ import (
 
 func TestListVNGsFromClusterName(t *testing.T) {
 	testCases := []struct {
-		description  string
-		clusterName  string
-		expectedVNGs []*oceanaws.LaunchSpec
+		description                string
+		clusterName                string
+		mockListClusters           func(ctx context.Context, listClusterInput *oceanaws.ListClustersInput) (*oceanaws.ListClustersOutput, error)
+		mockListLaunchSpecs        func(ctx context.Context, listLaunchSpecsInput *oceanaws.ListLaunchSpecsInput) (*oceanaws.ListLaunchSpecsOutput, error)
+		expectedVNGs               []*oceanaws.LaunchSpec
+		expectFailErrorClusters    bool
+		expectFailErrorLaunchSpecs bool
 	}{
 		{
-			description: "Should return all VNGs giving a cluster name",
+			description: "should return all VNGs giving a cluster name",
 			clusterName: "test",
+			mockListClusters: func(ctx context.Context, listClusterInput *oceanaws.ListClustersInput) (*oceanaws.ListClustersOutput, error) {
+				return &oceanaws.ListClustersOutput{
+					Clusters: []*oceanaws.Cluster{
+						{
+							ID:                  aws.String("o-1"),
+							ControllerClusterID: aws.String("test"),
+						},
+					},
+				}, nil
+			},
+			mockListLaunchSpecs: func(ctx context.Context, listLaunchSpecsInput *oceanaws.ListLaunchSpecsInput) (*oceanaws.ListLaunchSpecsOutput, error) {
+				return &oceanaws.ListLaunchSpecsOutput{
+					LaunchSpecs: []*oceanaws.LaunchSpec{
+						{
+							ID:      aws.String("1"),
+							Name:    aws.String("vng-test"),
+							OceanID: aws.String("o-1"),
+						},
+					},
+				}, nil
+			},
 			expectedVNGs: []*oceanaws.LaunchSpec{
 				{
 					ID:      aws.String("1"),
@@ -27,6 +53,49 @@ func TestListVNGsFromClusterName(t *testing.T) {
 					OceanID: aws.String("o-1"),
 				},
 			},
+		},
+		{
+			description: "should fail if can't retrieve clusters",
+			mockListClusters: func(ctx context.Context, listClusterInput *oceanaws.ListClustersInput) (*oceanaws.ListClustersOutput, error) {
+				return nil, fmt.Errorf("error")
+			},
+			expectFailErrorClusters: true,
+		},
+		{
+			description: "should fail if can't retrieve lauchspecs",
+			clusterName: "test",
+			mockListClusters: func(ctx context.Context, listClusterInput *oceanaws.ListClustersInput) (*oceanaws.ListClustersOutput, error) {
+				return &oceanaws.ListClustersOutput{
+					Clusters: []*oceanaws.Cluster{
+						{
+							ID:                  aws.String("o-1"),
+							ControllerClusterID: aws.String("test"),
+						},
+					},
+				}, nil
+			},
+			mockListLaunchSpecs: func(ctx context.Context, listLaunchSpecsInput *oceanaws.ListLaunchSpecsInput) (*oceanaws.ListLaunchSpecsOutput, error) {
+				return nil, fmt.Errorf("error")
+			},
+			expectFailErrorLaunchSpecs: true,
+		},
+		{
+			description: "should fail if cluster is not in the clusterList",
+			clusterName: "test2",
+			mockListClusters: func(ctx context.Context, listClusterInput *oceanaws.ListClustersInput) (*oceanaws.ListClustersOutput, error) {
+				return &oceanaws.ListClustersOutput{
+					Clusters: []*oceanaws.Cluster{
+						{
+							ID:                  aws.String("o-1"),
+							ControllerClusterID: aws.String("test"),
+						},
+					},
+				}, nil
+			},
+			mockListLaunchSpecs: func(ctx context.Context, listLaunchSpecsInput *oceanaws.ListLaunchSpecsInput) (*oceanaws.ListLaunchSpecsOutput, error) {
+				return nil, nil
+			},
+			expectFailErrorClusters: true,
 		},
 	}
 	RegisterFailHandler(Fail)
@@ -38,31 +107,17 @@ func TestListVNGsFromClusterName(t *testing.T) {
 
 			fakeOceanClient := &fakeocean.MockOceanCloudProviderAWS{}
 
-			fakeOceanClient.MockListClusters = func(ctx context.Context, listClusterInput *oceanaws.ListClustersInput) (*oceanaws.ListClustersOutput, error) {
-				return &oceanaws.ListClustersOutput{
-					Clusters: []*oceanaws.Cluster{
-						{
-							ID:                  aws.String("o-1"),
-							ControllerClusterID: aws.String("test"),
-						},
-					},
-				}, nil
-			}
+			fakeOceanClient.MockListClusters = tc.mockListClusters
+			fakeOceanClient.MockListLaunchSpecs = tc.mockListLaunchSpecs
 
-			fakeOceanClient.MockListLaunchSpecs = func(ctx context.Context, listLaunchSpecsInput *oceanaws.ListLaunchSpecsInput) (*oceanaws.ListLaunchSpecsOutput, error) {
-				return &oceanaws.ListLaunchSpecsOutput{
-					LaunchSpecs: []*oceanaws.LaunchSpec{
-						{
-							ID:      aws.String("1"),
-							Name:    aws.String("vng-test"),
-							OceanID: aws.String("o-1"),
-						},
-					},
-				}, nil
-			}
 			listLaunchSpec, err := ListVNGsFromClusterName(ctx, fakeOceanClient, tc.clusterName)
-			g.Expect(err).To(BeNil())
-			g.Expect(tc.expectedVNGs).To(BeEquivalentTo(listLaunchSpec))
+			if tc.expectFailErrorClusters || tc.expectFailErrorLaunchSpecs {
+				g.Expect(err).To(Not(BeNil()))
+				g.Expect(listLaunchSpec).To(BeNil())
+			} else {
+				g.Expect(err).To(BeNil())
+				g.Expect(tc.expectedVNGs).To(BeEquivalentTo(listLaunchSpec))
+			}
 		})
 	}
 
