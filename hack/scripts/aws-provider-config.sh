9fd55480e3558a70c39ed3206837c9422b4840b3
Carlos Martins <carlos.martins@MacBook-Pro-de-Carlos.local>
2024-01-18 18:17:13 -0300

    chore: sg for testing

--- hack/scripts/aws-provider-config.sh
+++ hack/scripts/aws-provider-config.sh
@@ -6,9 +6,8 @@ aws_access_key_id="${AWS_ACCESS_KEY_ID}"
 aws_secret_access_key="${AWS_SECRET_ACCESS_KEY}"
 aws_session_token="${AWS_SESSION_TOKEN}"
 aws_full_credentials="[default]
-aws_access_key_id=${aws_access_key_id}
-aws_secret_access_key=${aws_secret_access_key}
-aws_session_token=${aws_session_token}"
+aws_access_key_id = ${aws_access_key_id}
+aws_secret_access_key = ${aws_secret_access_key}"
 
 base64encode() {
   if [[ "$OSTYPE" == "darwin"* ]]; then
@@ -19,8 +18,8 @@ base64encode() {
 }
 
 # sed -e "s/aws_full_credentials/$(echo "${aws_full_credentials}" | base64encode)/g" \
-sed -e "s/aws_full_credentials/$(echo "${aws_full_credentials}" | base64encode)/g" \
-    -e "s/aws_access_key_id/$(echo "${aws_access_key_id}" | base64encode)/g" \
-    -e "s/aws_secret_access_key/$(echo "${aws_secret_access_key}" | base64encode)/g" \
+sed -e "s/aws_full_credentials/$(echo -n "${aws_full_credentials}" | base64encode)/g" \
+    -e "s/aws_access_key_id/$(echo -n "${aws_access_key_id}" | base64encode)/g" \
+    -e "s/aws_secret_access_key/$(echo -n "${aws_secret_access_key}" | base64encode)/g" \
     ./hack/assets/dependencies/aws-credentials.yaml.tpl \
     | kubectl apply -f -
