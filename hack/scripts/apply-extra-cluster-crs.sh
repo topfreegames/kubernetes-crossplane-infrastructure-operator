ae478a7465a4900f1d85384a9f6ecd59e5caf275
Carlos Martins <carlos.martins@MacBook-Pro-de-Carlos.local>
2024-01-18 18:17:13 -0300

    chore: sg for testing

---
+++ hack/scripts/apply-extra-cluster-crs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ -z "${KOPS_DOMAIN}" ]]; then
+  echo "Define which Route53 Domain is going to be used defining KOPS_DOMAIN as environment variable i.e.: dev.example.com"
+  exit 1
+fi
+
+if [[ -z "${KOPS_BUCKET}" ]]; then
+  echo "Define which S3 Bucket is going to be used defining KOPS_BUCKET as environment variable i.e.: kubernetes-test-bucket"
+  exit 1
+fi
+
+if [[ -z "${KOPS_ZONE}" ]]; then
+  echo "Define which zone is going to be used defining KOPS_ZONE as environment variable i.e.: us-east-1a"
+  exit 1
+fi
+
+cat hack/assets/cr/extra/test-cluster-a-mp-extra.yaml |  sed  "s/<ZONE>/${KOPS_ZONE}/g" | sed  "s/<DOMAIN>/${KOPS_DOMAIN}/g" | sed "s/<BUCKET>/${KOPS_BUCKET}/g" | kubectl apply -f - --context kind-$1
